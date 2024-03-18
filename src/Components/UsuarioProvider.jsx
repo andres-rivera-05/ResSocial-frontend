a3aeedce959bde29f795881a04a154b9e5e7c2a8
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-03-18 11:32:32 -0600

    Capturando el usuario y guardandolo en el localsorage

--- src/Components/UsuarioProvider.jsx
+++ src/Components/UsuarioProvider.jsx
@@ -1,12 +1,25 @@
-import React,{ createContext, useState } from 'react'
+import React,{ createContext, useState, useEffect } from 'react'
 
 const UserContext = createContext();
 
 export const UsuarioProvider = ({children}) => {
     const [ usuario, setUsuario]= useState(null)
 
+     useEffect(() => {
+        const storedUsuario = localStorage.getItem('usuario');
+        if (storedUsuario) {
+            setUsuario(JSON.parse(storedUsuario));
+        }
+    }, []);
+
+    // Función para actualizar el usuario y guardar en el localStorage
+    const actualizarUsuario = (nuevoUsuario) => {
+        setUsuario(nuevoUsuario);
+        localStorage.setItem('usuario', JSON.stringify(nuevoUsuario));
+    };
+
   return (
-      <UserContext.Provider value={{ usuario, setUsuario }}>
+      <UserContext.Provider value={{ usuario, actualizarUsuario }}>
           {children}
       </UserContext.Provider>
   )
